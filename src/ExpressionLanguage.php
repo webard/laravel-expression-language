361f5ea25a66ad717626cb233b670b445bac38ce
Bartłomiej Gajda <b.gajda@webard.me>
2024-04-01 19:57:15 +0200

    wip

--- src/ExpressionLanguage.php
+++ src/ExpressionLanguage.php
@@ -5,27 +5,23 @@ declare(strict_types=1);
 namespace Webard\LaravelExpressionLanguage;
 
 use Illuminate\Config\Repository;
+use Illuminate\Support\Traits\ForwardsCalls;
 use Symfony\Component\ExpressionLanguage\ExpressionFunction;
 use Symfony\Component\ExpressionLanguage\ExpressionFunctionProviderInterface;
 use Symfony\Component\ExpressionLanguage\ExpressionLanguage as SymfonyExpressionLanguage;
 
+/**
+ * @mixin SymfonyExpressionLanguage
+ */
 final class ExpressionLanguage
 {
+    use ForwardsCalls;
+
     public readonly SymfonyExpressionLanguage $expressionLanguage;
 
     public function __construct(private Repository $config)
     {
-        $allowedFunctions = $this->config->get('expression-language.allowed_functions', []);
-
-        $expressionFunctions = array_map(
-            function (string|array $functionName) {
-                $functionName = is_array($functionName) ? $functionName : [$functionName];
-
-                return ExpressionFunction::fromPhp(...$functionName);
-            },
-            $allowedFunctions
-        );
-        $functionsProvider = new class($expressionFunctions) implements ExpressionFunctionProviderInterface
+        $functionsProvider = new class($this->getExpressionFunctions()) implements ExpressionFunctionProviderInterface
         {
             public function __construct(private array $expressionFunctions)
             {
@@ -40,23 +36,22 @@ final class ExpressionLanguage
         $this->expressionLanguage = new SymfonyExpressionLanguage(null, [$functionsProvider]);
     }
 
-    /**
-     * Evaluates an expression.
-     *
-     * @param  array<string,mixed>  $values
-     */
-    public function evaluate(string $expression, array $values): mixed
+    private function getExpressionFunctions(): array
     {
-        return $this->expressionLanguage->evaluate($expression, $values);
+        $allowedFunctions = $this->config->get('expression-language.allowed_functions', []);
+
+        return array_map(
+            function (string|array $functionName) {
+                $functionName = is_array($functionName) ? $functionName : [$functionName];
+
+                return ExpressionFunction::fromPhp(...$functionName);
+            },
+            $allowedFunctions
+        );
     }
 
-    /**
-     * Compiles an expression source code.
-     *
-     * @param  array<string>  $names
-     */
-    public function compile(string $expression, array $names): string
+    public function __call(string $name, array $arguments): mixed
     {
-        return $this->expressionLanguage->compile($expression, $names);
+        return $this->forwardCallTo($this->expressionLanguage, $name, $arguments);
     }
 }
